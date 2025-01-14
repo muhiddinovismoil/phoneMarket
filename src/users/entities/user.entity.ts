ea9dd302209e06ef42cbdd2bf7cf9e7f2d28ce5b
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-15 00:04:41 +0500

    table relations successfully added

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -6,21 +6,31 @@ import { Orders } from '../../orders/entities/order.entity';
 @Entity()
 export class Users {
   @PrimaryGeneratedColumn('uuid')
-  @ApiProperty()
+  @ApiProperty({ example: '5asy6132bkjasd8f' })
   id: string;
   @Column({ length: 50 })
+  @ApiProperty({ example: 'John Doe' })
   fullname: string;
   @Column({ length: 50, unique: true })
+  @ApiProperty({ example: 'exampleemail@gmail.com' })
   email: string;
   @Column({ length: 50, unique: true })
-  phone: string;
+  @ApiProperty({ example: '+988990000000' })
+  phone_number: string;
   @Column({ length: 50 })
+  @ApiProperty({ example: 'qwerty12345' })
   password: string;
   @Column({ type: 'enum', enum: Role, default: Role.user })
+  @ApiProperty({ example: Role.user })
   role: Role;
   @Column({ default: false })
+  @ApiProperty({ example: false })
   isActive: boolean;
   @Column()
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NzdjMzg3YjI5YWMwZWI1Yjg1OGRiZDMiLCJlbWFpbCI6Im11aGlkZGlub3Zpc21vaWwy',
+  })
   refresh_token: string;
   @ManyToOne(() => Orders, (order) => order.users)
   orders: Orders[];
