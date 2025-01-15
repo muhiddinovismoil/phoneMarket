7c3c561e1e5331b15482c047cfc0b7d16d5fb42e
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-15 22:45:26 +0500

    guards successfully created and finished

---
+++ src/guards/role.guard.ts
@@ -0,0 +1,27 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../constants/decorator/role.decorator';
+import { Role } from '../constants/enums/roles';
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('You have no access. Permission Denied');
+    }
+    return true;
+  }
+}
